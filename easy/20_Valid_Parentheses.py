f5ccc4dc2947f56513b48f08855252db1e411757
Rich_Chigga <45484183+manhattan-chigga@users.noreply.github.com>
2021-04-01 20:24:20 +0800

    Update 20_Valid_Parentheses.py

--- easy/20_Valid_Parentheses.py
+++ easy/20_Valid_Parentheses.py
@@ -1,30 +1,55 @@
 """
-Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+Given an integer x, return true if x is palindrome integer.
 
-An input string is valid if:
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
 
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-Note that an empty string is also considered valid.
+
 
 Example 1:
 
-Input: "()"
+Input: x = 121
 Output: true
 Example 2:
 
-Input: "()[]{}"
-Output: true
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
 Example 3:
 
-Input: "(]"
+Input: x = 10
 Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 Example 4:
 
-Input: "([)]"
+Input: x = -101
 Output: false
-Example 5:
 
-Input: "{[]}"
-Output: true
+
+Constraints:
+
+-231 <= x <= 231 - 1
 """
+
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+        else:
+            str_x = str(x)
+            length = len(str_x)
+
+            count = 0
+            for i in range(length):
+                if str_x[i] != str_x[-(i+1)]:
+                    count += 1
+
+            if count != 0:
+                return False
+            else:
+                return True
+
+
+
+
+
